9de3b90e6b29fd58e5ea00d64313e5a5967043de
Osvaldo Borges <osvaldo.borges@ati.to.gov.br>
2023-06-12 17:51:57 -0300

    atualizando projeto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
 
-    def data_br(data_us)
-        date_us.strftime("%d/%m/%y")
-    end 
-
-
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+    def date_br(data_us)
+        data_us.strftime("%d/%m/%y")
+      end
+    
 end
